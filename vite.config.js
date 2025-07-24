5625d4fe31bf50dbc52246d2bfd1c7f726e206c3
Astordnomer <shivendrabhagat5@gmail.com>
2025-07-24 22:23:11 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Rosier-HackthonProject2025/',
+  base: '/Rosier/',
 })
